0045d8ad3db0167498d390c1e15618ea49683030
asmaquraishi <asmaq66@gmail.com>
2019-03-12 15:02:43 +0300

    comments feature Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_043840) do
+ActiveRecord::Schema.define(version: 2019_03_12_092558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2019_03_11_043840) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "song_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["song_id"], name: "index_comments_on_song_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -45,6 +56,8 @@ ActiveRecord::Schema.define(version: 2019_03_11_043840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "songs"
+  add_foreign_key "comments", "users"
   add_foreign_key "songs", "catogries", column: "catogrie_id"
   add_foreign_key "songs", "users"
 end
