97528df0a54291921fba551f6331900a49af65dd
“Majd_saleh” <mjd.s.m@hotmail.com>
2019-03-12 15:08:26 +0300

    likes feature Done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
    belongs_to :catogrie
    belongs_to :user
+   has_many :likes
    validates :title, :artist, :catogrie, :url, presence: true
    mount_uploader :url, AudioUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_043840) do
+ActiveRecord::Schema.define(version: 2019_03_12_070622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_03_11_043840) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "song_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["song_id"], name: "index_likes_on_song_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -45,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_03_11_043840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "songs"
+  add_foreign_key "likes", "users"
   add_foreign_key "songs", "catogries", column: "catogrie_id"
   add_foreign_key "songs", "users"
 end
